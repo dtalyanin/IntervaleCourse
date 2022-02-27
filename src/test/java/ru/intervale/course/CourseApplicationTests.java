79e52e002e0a1d08eb7843c11a0f28da68c2cabf
dtalyanin <dtalyanin@gmail.com>
2022-02-28 00:29:12 +0300

    Add Spring Initializr

---
+++ src/test/java/ru/intervale/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.intervale.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
